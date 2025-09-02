45e12f06fa7bd47fbb83570d9641eb2b8fcc82f8
willenius <wilhelm.bjor@gmail.com>
2025-09-02 12:07:20 +0200

    first push repo

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <h1> Hej </h1>
           {children}
         </body>
       </html>

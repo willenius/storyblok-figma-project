5b0c108171dd4e737c2c5cb93a6ec7c0a5905017
leorosendahl <leo.rosendahl@yh.nackademin.se>
2025-09-03 12:06:34 +0200

    test again

--- src/components/sb/Feature.js
+++ src/components/sb/Feature.js
@@ -8,7 +8,7 @@ export default function Feature({ blok }) {
       className="feature shadow-md p-4 rounded-md w-75"
     >
       <img
-        src={`https://www.skk.se/contentassets/bd4c36b7031a452f844c977f3d115e14/mediaflow-shibavalp-gulli-9928.jpg=${blok._uid}`}
+        src={`https://picsum.photos/250/250?random=${blok._uid}`}
         alt={blok.name}
         className="w-full aspect-square object-cover rounded-md"
       />

3e0cbe872536456651efc1c902c4a3d4a8341e13
willenius <wilhelm.bjor@gmail.com>
2025-09-04 11:08:15 +0200

    push updated grids

--- src/components/sb/Grid.js
+++ src/components/sb/Grid.js
@@ -2,14 +2,22 @@ import { storyblokEditable } from "@storyblok/react";
 import ServerComponent from "./ServerComponent";
 
 export default function Grid({ blok }) {
-
   return (
     <div
       {...storyblokEditable(blok)}
-      className="grid gap-4 w-full grid-cols-[repeat(auto-fit,minmax(15.625rem,1fr))]"
+      className="grid gap-4 w-full justify-items-center grid-cols-[repeat(auto-fit,minmax(12.625rem,1fr))]"
     >
-      {blok.columns.map((nestedBlok) => (
-        <ServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+      {blok.columns.map((nestedBlok, index) => (
+        <div
+          key={nestedBlok._uid}
+          className={
+            index === Math.floor(blok.columns.length / 2)
+              ? "-translate-y-1/4 relative"
+              : ""
+          }
+        >
+          <ServerComponent blok={nestedBlok} />
+        </div>
       ))}
     </div>
   );

06e751a5abbd94a8badcc92f554414ff6f127917
bjornwildenstam <bjorn.wildenstam@yh.nackademin.se>
2025-09-04 11:39:49 +0200

    First fix on products

---
+++ src/app/products/page.js
@@ -0,0 +1,26 @@
+import { getStoryblokApi } from "@/lib/storyblok";
+import { StoryblokStory } from '@storyblok/react/rsc';
+
+/**
+ * Home page - renders the Storyblok story through which all components are rendered
+ * @returns {Promise<{story: StoryblokStory}>}
+ */
+export default async function Home() {
+  const { data } = await fetchData();
+  return (
+    <div className="page">
+      <StoryblokStory story={data.story} />
+    </div>
+  );
+}
+
+/**
+ * Fetch the data from Storyblok through a server action
+ * @returns {Promise<{story: StoryblokStory}>}
+ */
+export async function fetchData() {
+  const storyblokApi = getStoryblokApi();
+  return await storyblokApi.get("cdn/stories/products", {
+    version: "draft",
+  });
+}

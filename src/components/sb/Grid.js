911cbc291d54155f21662062aff8cab9640d3cad
willenius <wilhelm.bjor@gmail.com>
2025-09-04 11:38:05 +0200

    fixed more styling for grids.

--- src/components/sb/Grid.js
+++ src/components/sb/Grid.js
@@ -5,16 +5,13 @@ export default function Grid({ blok }) {
   return (
     <div
       {...storyblokEditable(blok)}
-      className="grid gap-4 w-full justify-items-center grid-cols-[repeat(auto-fit,minmax(12.625rem,1fr))]"
+      className="grid gap-2 w-full justify-items-center grid-cols-[repeat(auto-fit,minmax(12.625rem,1fr))]"
     >
       {blok.columns.map((nestedBlok, index) => (
         <div
           key={nestedBlok._uid}
-          className={
-            index === Math.floor(blok.columns.length / 2)
-              ? "-translate-y-1/4 relative"
-              : ""
-          }
+          className={`flex items-center justify-center w-128 h-128 rounded-lg overflow-hidden shadow-md 
+            ${index === Math.floor(blok.columns.length / 2) ? "-translate-y-1/4 relative" : ""}`}
         >
           <ServerComponent blok={nestedBlok} />
         </div>

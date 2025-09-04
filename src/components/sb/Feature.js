ef83248c2539c10571a1001f3e3b2d73c7188aa0
leorosendahl <leo.rosendahl@yh.nackademin.se>
2025-09-04 11:09:35 +0200

    added image.filename to feature.js

--- src/components/sb/Feature.js
+++ src/components/sb/Feature.js
@@ -2,18 +2,16 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 import ServerComponent from "./ServerComponent";
 
 export default function Feature({ blok }) {
+  console.log("blok", blok)
 
   return (
     <div
       {...storyblokEditable(blok)}
       className="feature shadow-md p-4 rounded-md w-75"
     >
-      { <img
-        src={`https://picsum.photos/250/250?random=${blok._uid}`}
-        alt={blok.name}
-        className="w-full aspect-square object-cover rounded-md"
-      /> }
+      <img src={blok.image.filename} alt="Image"/>
       <h2 className="text-2xl font-bold">{blok.name}</h2>
+      <img src={blok?.name.filename}/>
       <p className="text-gray-600">{blok.description}</p>
       <div className="flex flex-col gap-2">
         {blok?.cta?.map((nestedBlok) => (

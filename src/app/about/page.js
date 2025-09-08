89bfd19df7d642a1efa16540b4e3bbddfee6b9b8
leorosendahl <leo.rosendahl@yh.nackademin.se>
2025-09-08 14:59:24 +0200

    .

---
+++ src/app/about/page.js
@@ -0,0 +1,19 @@
+import { getStoryblokApi } from "@/lib/storyblok";
+import { StoryblokStory } from '@storyblok/react/rsc';
+
+//Example of a static page
+export default async function Home() {
+  const { data } = await fetchData();
+  return (
+    <div className="page">
+      <StoryblokStory story={data.story} />
+    </div>
+  );
+}
+
+export async function fetchData() {
+  const storyblokApi = getStoryblokApi();
+  return await storyblokApi.get("cdn/stories/about", {
+    version: "draft",
+  });
+}

77bc34ce00a37bc8aeac561c9c157ffe9a520fb4
willenius <wilhelm.bjor@gmail.com>
2025-09-08 15:54:45 +0200

    Pushing up dummy-files for robots + sitemap

---
+++ src/app/robots.js
@@ -0,0 +1,11 @@
+/* export default function robots() {
+    const baseUrl = 'https://nackademin-webb-24-cms-lessons.vercel.app'
+    return {
+      rules: {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/global/',
+      },
+      sitemap: `${baseUrl}/sitemap.xml`,
+    }
+  } */

a658596647caf8ee1b34693832fab149372a4f5a
willenius <wilhelm.bjor@gmail.com>
2025-09-08 14:37:24 +0200

    updated grid & hero.js

--- src/components/sb/Grid.js
+++ src/components/sb/Grid.js
@@ -3,38 +3,37 @@ import ServerComponent from "./ServerComponent";
 import { getStoryblokApi } from "@/lib/storyblok";
 
 export default async function Grid({ blok }) {
-  const response = await getProducts()
-  console.log("product", response.data);
+  const response = await getProducts();
+  console.log("price", response.data.content);
 
   return (
     <div
       {...storyblokEditable(blok)}
-      className="grid gap-2 justify-items-center grid-cols-[repeat(auto-fit,minmax(12.625rem,1fr))]"
+      className="flex flex-row gap-4 overflow-x-auto py-4"
     >
-      <div className="home-grid">
-        {response.data.stories.map((p) => (
-          <div key={p.id} className="homeProducts">
-            <img src={p.content.image.filename} alt="pictures of shoes" />
-            <p className="productTitle">{p.content.title}</p>
-            </div>
-        ))}
-      {/* {blok.columns.map((nestedBlok, index) => (
+      {response.data.stories.map((p) => (
         <div
-          key={nestedBlok._uid}
-          className={`flex items-center justify-center w-128 h-128 rounded-lg overflow-hidden shadow-md 
-            ${index === Math.floor(blok.columns.length / 2) ? "-translate-y-1/4 relative" : ""}`}
+          key={p.id}
+          
+          className="flex flex-col items-center justify-center w-48 h-48 rounded-lg overflow-hidden shadow-md"
         >
-          <ServerComponent blok={nestedBlok} />
-        </div> */}
-      {/* ))} */}
+          <img
+            src={p.content.image.filename}
+            alt={p.content.title}
+            className="w-full h-full object-cover"
+          />
+          <p className="productTitle mt-2 text-center">{p.content.title}</p>
+          <p>{p.content.price}</p>
+        </div>
+      ))}
     </div>
-    </div>
-  )};
-
-async function getProducts () {
+  );
+}
+async function getProducts() {
   const storyblokApi = getStoryblokApi();
   return await storyblokApi.get("cdn/stories/", {
     version: "draft",
-    content_type: 'product', per_page: 3
+    content_type: "product",
+    per_page: 3,
   });
 }

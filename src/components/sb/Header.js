4f79fb5f1c0317ef921973c17016eaaad35ef9e7
leorosendahl <leo.rosendahl@yh.nackademin.se>
2025-09-09 16:57:02 +0200

    d

--- src/components/sb/Header.js
+++ src/components/sb/Header.js
@@ -4,6 +4,7 @@ export default function Header({ blok }) {
   let heroClasses =
     "h-[20vh] flex flex-col justify-center items-center text-center gap-4 bg-gray-100 shadow";
 
+    
   return (
     <header {...storyblokEditable(blok)} className={heroClasses}>
       {/* Visa loggan om den finns */}
--- src/components/sb/Products_Hero.js
+++ src/components/sb/Products_Hero.js
@@ -4,7 +4,6 @@ import { storyblokEditable } from "@storyblok/react";
 export default function Products_Hero({ blok }) {
   let heroClasses = `h-[60vh] mt-[0px] mb-[0px] flex flex-col justify-center items-center text-center gap-4`;
   
-
   return (
     <div
     {...storyblokEditable(blok)}

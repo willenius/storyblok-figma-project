2b074fe22b5f2863cc660ced4c8015b4c5a0d7f3
willenius <wilhelm.bjor@gmail.com>
2025-09-09 10:37:49 +0200

    updating baseurl in robots

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,5 +1,5 @@
 export default function robots() {
-    const baseUrl = 'https://nackademin-webb-24-cms-lessons.vercel.app'
+    const baseUrl = 'https://storyblok-figma-project.vercel.app/'
     return {
       rules: {
         userAgent: '*',
